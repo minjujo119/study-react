1905ce98004f0bfbb2364853e71f130a81f20b4b
minjujo119 <minjujo119@gmail.com>
2024-10-06 16:20:08 -0400

    [update] 일기 추가,수정, 삭제 기능 업데이트

--- lecture01-react-one-bite/section09_Diary/src/App.jsx
+++ lecture01-react-one-bite/section09_Diary/src/App.jsx
@@ -1,5 +1,5 @@
 import './App.css'
-import { useReducer } from 'react'
+import { useReducer, useRef, createContext } from 'react'
 import { Route, Routes, Link, useNavigate } from 'react-router-dom'
 import Home from './pages/Home'
 import New from './pages/New'
@@ -7,17 +7,16 @@ import Diary from './pages/Diary'
 import Edit from './pages/Edit'
 import NotFound from './pages/Notfound'
 
-
 const mockData = [
   {
     id: 1,
-    createdDate: new Date().getTime,
+    createdDate: new Date().getTime(),
     emotionId: 1,
     content: "1번 일기 내용"
   },
   {
     id: 2,
-    createdDate: new Date().getTime,
+    createdDate: new Date().getTime(),
     emotionId: 2,
     content: "2번 일기 내용"
   }
@@ -25,23 +24,70 @@ const mockData = [
 ]
 
 function reducer(state, action) {
-  return state;
+  switch(action.type){
+    case 'CREATE': return [...state, action.data];
+    case 'UPDATE': return (
+      state.map((item)=>
+      String(item.id) === String(action.data.id)
+      ? action.data 
+      : item
+    ));
+    case 'DELETE': return (
+      state.filter((item)=>
+        String(item.id) !== String(action.data)
+      )
+    )
+    default:
+      return state;
+  }
 }
+
+const DiaryStateContext = createContext();
+const DiaryDispatchContext = createContext();
 
 function App() {
 
   const [data, dispatch] = useReducer(reducer, mockData);
+  const idRef = useRef(3);
+
+  // 일기 추가
+  const onCreate =(createdDate,emotionId,content)=>{
+    dispatch({
+      type: "CREATE",
+      data: {
+        id: idRef.current++,
+        createdDate,
+        emotionId,
+        content,
+      }
+    })
+  }
+  const onUpdate=(id,createdDate,emotionId,content)=>{
+    dispatch({
+      type: "UPDATE",
+      data: {id,createdDate,emotionId,content}
+    })
+  }
+  const onDelete=(id)=>{
+    dispatch({
+      type: "DELETE",
+      data: id
+    })
+  }
 
   return (
-    <>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/new" element={<New/>} />
-        <Route path="/diary/:id" element={<Diary/>} />
-        <Route path="/edit/:id" element={<Edit/>} />
-        <Route path="*" element={<NotFound/>} />
-
-      </Routes>
+    <>      
+      <DiaryStateContext.Provider value={data}>
+        <DiaryDispatchContext.Provider value={{onCreate,onUpdate,onDelete}}>
+          <Routes>
+            <Route path="/" element={<Home/>} />
+            <Route path="/new" element={<New/>} />
+            <Route path="/diary/:id" element={<Diary/>} />
+            <Route path="/edit/:id" element={<Edit/>} />
+            <Route path="*" element={<NotFound/>} />
+          </Routes>
+        </DiaryDispatchContext.Provider>
+      </DiaryStateContext.Provider>
     </>
   )
 }
